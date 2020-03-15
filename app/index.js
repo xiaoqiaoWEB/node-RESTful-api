7c00d6f5745a199291cf5a5c05e67aeed7759ef3
xiaoqiaoWEB <346043591@qq.com>
2020-03-15 22:15:28 +0800

    上传文件

--- app/index.js
+++ app/index.js
@@ -1,12 +1,18 @@
 const Koa = require('koa')
 const app = new Koa()
-const bodyparser  = require('koa-bodyparser')
+//const bodyparser  = require('koa-bodyparser')
 const routing = require('./routes');
 const mongoose = require('mongoose');
+const koaBody = require('koa-body')
+const path = require('path')
+const koaStatic = require('koa-static')
 
 mongoose.connect('mongodb://localhost/zhihu', { useUnifiedTopology: true }, () => {
   console.log('数据库启动成功')
 });
+
+// 静态服务
+app.use(koaStatic(path.join(__dirname, 'public')))
 
 // 错误处理
 const error = require('koa-json-error')
@@ -19,7 +25,14 @@ app.use(error({
   postFormat: (e, {stack, ...rest}) =>  process.env.NODE_ENV === 'production' ? rest : {stack, ...rest}
 }))
 
-app.use(bodyparser())
+// 配置上传文件的 中间件
+app.use(koaBody({
+  multipart: true,
+  formidable: {
+    uploadDir: path.join(__dirname, '/public/uploads'),
+    keepExtensions: true, // 保留文件的 后缀名
+  }
+}))
 
 // 批量注册路由
 routing(app);
