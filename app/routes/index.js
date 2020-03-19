abb294c0fead676fa2ee79ef92bcf8c94f8990c9
xiaoqiaoWEB <346043591@qq.com>
2020-03-19 23:52:48 +0800

    逛逛

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,6 @@
 const fs = require('fs')
 
 module.exports = (app) => {
-  //console.log(fs.readdirSync(__dirname))
   fs.readdirSync(__dirname).forEach(file => {
     if(file == 'index.js') {return;}
     let route = require(`./${file}`)
